e7e13a6eb07ee9817a0190375d232843025841a9
Gemorroj <wapinet@gmail.com>
2015-12-12 13:33:11 +0300

    add php 7.0 to travis

--- Rss.php
+++ Rss.php
@@ -1,16 +1,12 @@
 <?php
 /**
- *
  * This software is distributed under the GNU GPL v3.0 license.
  *
  * @author    Gemorroj
- * @copyright 2012 http://wapinet.ru
+ * @copyright 2015 http://wapinet.ru
  * @license   http://www.gnu.org/licenses/gpl-3.0.txt
  * @link      https://github.com/Gemorroj/Rss
- * @version   0.2
- *
  */
-
 class Rss extends DOMDocument
 {
     /**
---
+++ Tests/RssTest.php
@@ -0,0 +1,8 @@
+<?php
+class RssTest extends PHPUnit_Framework_TestCase
+{
+    public function testRss()
+    {
+        self::markTestSkipped('Not implemented yet');
+    }
+}
